69156f6ec762186f7b9536d85235202a8fbacc14
Thalles Fraga <141372191+thallesfsilva@users.noreply.github.com>
2025-05-29 09:45:42 -0300

    Update settings.py

--- clinica/clinica/settings.py
+++ clinica/clinica/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "chave-insegura-para-dev")
 
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = ['clinicconnect.app.br', 'your-render-subdomain.onrender.com']
+ALLOWED_HOSTS = ['clinicconnect.app.br', 'minhaclinicapython.onrender.com']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
